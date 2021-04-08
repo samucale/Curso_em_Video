fcfe0435f5260f523ac15fb5c3b485d50c6653dc
Samuel Antonio Silva <73207817+samucale@users.noreply.github.com>
2021-04-08 00:45:19 -0300

    .

--- massa corporea.py
+++
@@ -1,7 +0,0 @@
-peso = float(input('Qual é seu peso ? (kg)'))
-altura = float (input('Qual é sua altura ? (m)'))
-imc = peso / (altura ** 2)
-print ('O imc dessa pessoa é de {:.1f}'.format(imc))
-
-###adicionando linha 
-Print ("Linha")
